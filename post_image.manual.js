505b788f62537c200dbe3c163bb4214c145e19b7
ryanj <ryan.jarvinen@gmail.com>
2015-05-14 13:26:04 -0700

    put different things in different places

--- post_image.manual.js
+++ post_image.manual.js
@@ -1,11 +1,14 @@
 var request = require('request')
   , fs = require('fs')
-  , server_name = process.env.OPENSHIFT_SERVER || "http://localhost:8080";
+  , server_name = process.env.OPENSHIFT_SERVER || "http://localhost:8080"
+  , username = process.env.USERNAME || "jefferson"
+  , cuid = process.env.CUID || "007"
+  , submission = process.env.SUBMISSION || "awesome"
 
 
 var readStream = fs.createReadStream('static/img/cherries.png');
 var png = fs.readFileSync(__dirname + '/static/img/cherries.png');
-var url = server_name + '/doodle?name=John%20Doe&cuid=007&submission=awesome';
+var url = server_name + '/doodle?username='+username+'&cuid='+cuid+'&submission='+submission;
 
 readStream.pipe(request.put(url, function (err, res, body) {
   if (err) {
@@ -13,4 +16,3 @@ readStream.pipe(request.put(url, function (err, res, body) {
   }
   console.log('res', res.body);
 }));
-//readStream.pipe(req);
