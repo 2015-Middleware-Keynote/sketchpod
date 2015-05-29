e5c81379f9142933adee534d4a5b00f872cbc07b
Brian Leathem <bleathem@gmail.com>
2015-05-28 20:26:41 -0700

    Cleanup, consolidating files, and removing unused code

--- server.js
+++ server.js
@@ -1,33 +1,58 @@
 var cc       = require('config-multipaas'),
     fs       = require('fs'),
-    restify  = require('restify'),
-    doodleController = require('./doodle_controller.js')
+    os       = require('os'),
+    restify  = require('restify')
+    querystring = require('querystring');
 
-var app      = restify.createServer()
+var app = restify.createServer();
 
-// Default state:
-var unknown_client_id = 'unknown client id'
-var unknown_user_name = 'unclaimed'
-var unknown_submission= 'no doodle'
+var config = cc();
 
-var config   = cc().add({
-  CUID : process.env.CUID || unknown_client_id,
-  USERNAME : process.env.USERNAME || unknown_user_name,
-  SUBMISSION : process.env.SUBMISSION || unknown_submission
-});
-var index = fs.readFileSync(__dirname + '/index.html');
-var cuid = false;
-var submission = false;
-var username = false;
+var sketch = {
+  cuid : process.env.CUID || 'unknown client id',
+  username : process.env.USERNAME || 'unclaimed',
+  submission : process.env.SUBMISSION || 'no doodle'
+};
+
+var index = fs.readFileSync(__dirname + '/static/index.html');
+
+var receiveImage = function(req, res, next) {
+  var data = new Buffer('');
+  req.on('data', function(chunk) {
+    data = Buffer.concat([data, chunk]);
+  });
+  req.on('error', function(err) {
+    next(err);
+  });
+  req.on('end', function() {
+    var filename = './static/img/doodle.png';
+    fs.open(filename, 'w', function(err, fd) {
+      if (err) {
+        next(err);
+      };
+      var query = querystring.parse(req.query());
+      fs.write(fd, data, 0, data.length, 0, function(err, written, buffer) {
+        if (err) {
+          next(err);
+        };
+        sketch.url = 'http://localhost:8080/img/doodle.png';
+        sketch.username = query.username;
+        sketch.cuid = query.cuid;
+        sketch.submission = query.submission;
+        return res.json(sketch);
+      });
+    })
+  });
+}
 
 // Routes
 app.get('/status', function (req, res, next) { res.send("{status: 'ok'}"); });
-app.put('/doodle', doodleController.receiveImage);
-app.get('/', function (req, res, next){
+app.put('/doodle', receiveImage);
+app.get('/', function (req, res, next) {
   var html  = index.toString()
-             .replace( /\{\{SUBMISSION\}\}/, submission || config.get('SUBMISSION'))
-             .replace( /\{\{USERNAME\}\}/, username || config.get('USERNAME'))
-             .replace( /\{\{CUID\}\}/, cuid || config.get('CUID'))
+             .replace( /\{\{SUBMISSION\}\}/, sketch.submission)
+             .replace( /\{\{USERNAME\}\}/, sketch.username)
+             .replace( /\{\{CUID\}\}/, sketch.cuid)
   res.status(200);
   res.header('Content-Type', 'text/html');
   res.end(html.replace(/host:port/g, req.header('Host')));
@@ -39,52 +64,3 @@ app.get('.*', restify.serveStatic({directory: './static/'}));
 app.listen(config.get('PORT'), config.get('IP'), function () {
   console.log( "Listening on " + config.get('IP') + ", port " + config.get('PORT') )
 });
-
-// Sockets:
-var WebSocketServer = require('ws').Server
-  , thousand = require('./thousand')
-var wss = new WebSocketServer({server: app, path: '/thousand'});
-var eventEmitter = thousand.doodleEmitter;
-var count = 0;
-var clients = {};
-
-wss.broadcast = function broadcast(data) {
-  for (var i in clients) {
-    var ws = clients[i];
-    if (ws.readyState === ws.OPEN) {
-      ws.send(data);
-    } else if (ws.readyState === ws.CLOSED) {
-      console.log('Peer #' + ws.id + ' disconnected from /thousand.');
-      delete clients[ws.id];
-    }
-  };
-};
-
-wss.on('connection', function connection(ws) {
-var id = count++;
-clients[id] = ws;
-ws.id = id;
-console.log('/thousand connection');
-var subscription;
-subscription = thousand.events.subscribe(function(event) {
-  if (ws.readyState === ws.OPEN) {
-    ws.send(JSON.stringify({type: 'event', data: event}));
-  };
-});
-ws.on('message', function(data, flags) {
-var message = JSON.parse(data);
-  if (message.type === 'ping') {
-    ws.send(JSON.stringify({type: 'pong'}));
-  }
-});
-ws.onclose = function() {
-  console.log('Onclose: disposing /thousand subscriptions');
-  subscription && subscription.dispose();
-};
-});
-
-eventEmitter.on('new-doodle', function(doodle) {
-console.log('doodle listener invoked.');
-  wss.broadcast(JSON.stringify({type: 'doodle', data: doodle}));
-});
-
